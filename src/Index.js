34289e8a8603aad423d66d836a3dc1b02b792f66
Javier1A2 <javiergp.dev@gmail.com>
2026-04-15 13:04:06 -0600

    chore: rename project files to UpperCamelCase

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import './Index.css';
 import App from './App';
-import LoginPage from './Login/login';
+import LoginPage from './Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
