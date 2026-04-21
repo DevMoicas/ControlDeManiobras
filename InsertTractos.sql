1bc94cb7ebe42c239dbc7904abd34e81cbdbeb59
Javier1A2 <javiergp.dev@gmail.com>
2026-04-21 12:33:12 -0600

    DB: separar anio y unidad en tractos

--- InsertTractos.sql
+++ InsertTractos.sql
@@ -1,12 +1,12 @@
-INSERT INTO tractos (no_eco, unidad, placas, tipo) VALUES
-('NO. 01', 'CASCADIA 2014', '21AX4Y', 'trailer'),
-('NO. 03', 'ISUZU 2013', '52BG9A', 'rabón'),
-('NO. 04', 'KANGOO 2010', 'FE9713A', 'X'),
-('NO. 06', 'M2 2013', '07AX1Y', 'thorton'),
-('NO. 07', 'NISSAN 2010', '30BA6K', '1-2 ton'),
-('NO. 05', 'FREIGHTLINER 2006', '64AK5A', 'trailer'),
-('NO. 02', 'INTERNATIONAL 2006', '38AV7C', 'trailer'),
-('NO. 08', 'KENWORTH 2012', '19BD1B', 'trailer'),
-('NO. 09', 'KENWORTH 2013', '06BF7E', 'trailer'),
-('NO.10', 'PEUGEOT 2024', 'JH5356A', '1 ton'),
-('NO.11', 'KENWORTH 2018', '88BK5H', 'trailer');
+INSERT INTO tractos (no_eco, anio, unidad, placas, tipo) VALUES
+('NO. 01', 2014, 'CASCADIA', '21AX4Y', 'trailer'),
+('NO. 03', 2013, 'ISUZU', '52BG9A', 'rabón'),
+('NO. 04', 2010, 'KANGOO', 'FE9713A', 'X'),
+('NO. 06', 2013, 'M2', '07AX1Y', 'thorton'),
+('NO. 07', 2010, 'NISSAN', '30BA6K', '1-2 ton'),
+('NO. 05', 2006, 'FREIGHTLINER', '64AK5A', 'trailer'),
+('NO. 02', 2006, 'INTERNATIONAL', '38AV7C', 'trailer'),
+('NO. 08', 2012, 'KENWORTH', '19BD1B', 'trailer'),
+('NO. 09', 2013, 'KENWORTH', '06BF7E', 'trailer'),
+('NO.10', 2024, 'PEUGEOT', 'JH5356A', '1 ton'),
+('NO.11', 2018, 'KENWORTH', '88BK5H', 'trailer');
