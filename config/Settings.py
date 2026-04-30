24f91605f270083f2394637e6f9f70f9ceb8d450
Ramon Peregrino <jesuslarios856@gmail.com>
2026-04-29 23:22:38 -0600

    actualización problema archivo

--- config/Settings.py
+++ config/Settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'fraba_erp'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'elastaxd'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '123456'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
